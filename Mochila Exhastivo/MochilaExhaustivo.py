5c73a63078700687d186e06dac455cdac232544e
lisandroferroni <lisandro.ferroni@gmail.com>
2018-07-17 18:36:36 -0300

    Nuevo

--- Mochila Exhastivo/MochilaExhaustivo.py
+++ Mochila Exhastivo/MochilaExhaustivo.py
@@ -7,18 +7,17 @@ class Elemento:
         self.valor=valor
 
 
-elemento=[10]
-elemento [0]=Elemento(150,20)
-elemento [1]=Elemento(325,40)
-elemento [2]=Elemento(600,50)
-elemento [3]=Elemento(805,36)
-elemento [4]=Elemento(430,25)
-elemento [5]=Elemento(1200,64)
-elemento [6]=Elemento(770,54)
-elemento [7]=Elemento(60,18)
-elemento [8]=Elemento(930,46)
-elemento [9]=Elemento(353,28)
+elemento=[]
+elemento.extend([Elemento(150,20)])
+elemento.extend([Elemento(325,40)])
+elemento.extend([Elemento(600,50)])
+elemento.extend([Elemento(805,36)])
+elemento.extend([Elemento(430,25)])
+elemento.extend([Elemento(1200,64)])
+elemento.extend([Elemento(770,54)])
+elemento.extend([Elemento(60,18)])
+elemento.extend([Elemento(930,46)])
+elemento.extend([Elemento(353,28)])
 volMax=4200
 
 subconjunto=[]
-
