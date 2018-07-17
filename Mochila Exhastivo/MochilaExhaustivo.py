26c187ed7c65a8442b1d9775012f4d845dd2501b
lisandroferroni <lisandro.ferroni@gmail.com>
2018-07-17 18:17:57 -0300

    Nuevo

---
+++ Mochila Exhastivo/MochilaExhaustivo.py
@@ -0,0 +1,24 @@
+import numpy as np 
+import random
+
+class Elemento:
+    def __init__(self,volumen,valor):
+        self.volumen=volumen
+        self.valor=valor
+
+
+elemento=[10]
+elemento [0]=Elemento(150,20)
+elemento [1]=Elemento(325,40)
+elemento [2]=Elemento(600,50)
+elemento [3]=Elemento(805,36)
+elemento [4]=Elemento(430,25)
+elemento [5]=Elemento(1200,64)
+elemento [6]=Elemento(770,54)
+elemento [7]=Elemento(60,18)
+elemento [8]=Elemento(930,46)
+elemento [9]=Elemento(353,28)
+volMax=4200
+
+subconjunto=[]
+
