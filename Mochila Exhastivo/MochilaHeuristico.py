b7a5855a3c22367b21e4450d9ab3f0930a69c1c7
Luciano Ezequiel Babaglio <luciano.babaglio@gmail.com>
2018-07-18 17:08:57 -0300

    Busqueda heuristca sale mal FAIL

---
+++ Mochila Exhastivo/MochilaHeuristico.py
@@ -0,0 +1,38 @@
+import numpy as np 
+class Elemento:
+    def __init__(self,volumen,valor):
+        self.volumen=volumen
+        self.valor=valor
+   
+def utilidad(e):
+    return (e.volumen/e.valor)
+
+
+def valor (s):
+    suma=0
+    for x in range (0, len (s)):
+        if s[x]==1:
+            suma=suma+elemento[x].valor
+    return suma
+
+def calcUtilidades(elementos):
+    utilidades=[]
+    for i in range (0,n):
+        utilidades.extend(utilidad(elementos[i]))
+    return utilidades 
+
+elemento=[]
+elemento.extend([Elemento(150,20)])
+elemento.extend([Elemento(325,40)])
+elemento.extend([Elemento(600,50)])
+elemento.extend([Elemento(805,36)])
+elemento.extend([Elemento(430,25)])
+elemento.extend([Elemento(1200,64)])
+elemento.extend([Elemento(770,54)])
+elemento.extend([Elemento(60,18)])
+elemento.extend([Elemento(930,46)])
+elemento.extend([Elemento(353,28)])
+volMax=4200
+n=10
+
+utilidades=calcUtilidades(elemento)
