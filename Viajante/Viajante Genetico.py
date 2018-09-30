5ba15391cec0a8b96fd395d5add0b9d1bcf31c99
lisandroferroni <lisandro.ferroni@gmail.com>
2018-09-30 04:03:17 -0300

    Inicio, una base

---
+++ Viajante/Viajante Genetico.py
@@ -0,0 +1,135 @@
+import numpy as np 
+import random
+import matplotlib.pyplot as plt
+from math import radians, sin, cos, sqrt, asin, atan, atan2
+class Cromosoma:
+    def __init__(self,trayectoria):
+        self.trayectoria = trayectoria
+        self.distancia = calculaDistanciaTotal(trayectoria)
+        self.fitness=-1
+    def getDistancia(self):
+        return self.distancia
+    def getFitness(self):
+        return self.fitness
+    def setFitness(self, fitness):
+        self.fitness=fitness    
+    def getTrayectoria(self):
+        return self.trayectoria
+
+class ciudad:
+    def __init__(self,nombre,lat,lon):
+        self.nombre=nombre
+        self.lat=lat
+        self.lon=lon
+        self.id=0    
+    def setId(self,id):
+        self.id=id
+    def getid(self):
+        return self.id
+    def getLat(self):
+        return self.lat
+    def getLon(self):
+        return self.lon
+
+def calculaDistanciaTotal(trayectoria):
+    distanciaTotal = 0
+    for x in range (0,23):
+        lat1 = ciudades[trayectoria[x]].getLat()
+        lon1 = ciudades[trayectoria[x]].getLon()
+        lat2 = ciudades[trayectoria[x+1]].getLat()
+        lon2 = ciudades[trayectoria[x+1]].getLon()
+        distanciaTotal = distanciaTotal + calc_distance(lat1, lon1, lat2, lon2)
+
+    return distanciaTotal
+
+def calc_distance(lat1, lon1, lat2, lon2): 
+    R = 6372.8 # Earth radius in kilometers
+
+    dLat = radians(lat2 - lat1)
+    dLon = radians(lon2 - lon1)
+    lat1 = radians(lat1)
+    lat2 = radians(lat2)
+ 
+    a = sin(dLat/2)**2 + cos(lat1)*cos(lat2)*sin(dLon/2)**2
+    c = 2*asin(sqrt(a))
+    return (R * c)
+
+def crearPoblacionInicial(num):
+    poblacionInicial = []
+    for _ in range (0,cantIndividuos):
+        trayectoria = None
+        trayectoria = []        #vacio trayectoria (sin esto quedan todas iguales)
+        for i in range (0,23):  
+            trayectoria.extend([i]) #crea una lista con enteros del 0 al 22
+        trayectoria.pop(num)        #Elimina el num de ciudad inicial de la trayectoria   
+        random.shuffle(trayectoria) #Mezcla trayectoria
+        trayectoria.insert(0,num)
+        trayectoria.extend([num])   #Ciudad inicial, al principio y al final de la trayectoria
+        poblacionInicial.extend([Cromosoma(trayectoria)])   #Crea nuevo cromosoma con trayectoria aleatoria
+    return poblacionInicial         #Devuelve lista con @cantIndividuos cromosomas 
+
+def calcularFitness (poblacion):
+    total=0.0
+    for x in range(0,len(poblacion)):
+        total = total + 1/poblacion[x].getDistancia()
+    for x in range(0,len(poblacion)):
+        poblacion[x].setFitness(int(round(100/poblacion[x].getDistancia()/total)))
+
+def crearRuleta(poblacion):
+    ruleta=[]
+    calcularFitness(poblacion)
+    for x in range (0,len(poblacion)):
+        for _ in range (0,poblacion[x].getFitness()):
+            ruleta.extend([poblacion[x].getTrayectoria()])
+    return ruleta
+
+ciudades=[]
+ciudades.extend([ciudad("Buenos Aires",-34.599722, -58.381944)])
+ciudades.extend([ciudad("Córdoba",-31.416667, -64.183333)])
+ciudades.extend([ciudad("S. F. del valle de Catamarca",-28.468611, -65.779167)])
+ciudades.extend([ciudad("Resistencia",-27.451389, -58.986667)])
+ciudades.extend([ciudad("Rawson",-43.3, -65.1)])
+ciudades.extend([ciudad("Corrientes",-27.483333, -58.816667)])
+ciudades.extend([ciudad("Paraná",-31.744444, -60.5175)])
+ciudades.extend([ciudad("Formosa",-26.184722, -58.175833)])
+ciudades.extend([ciudad("San S. de Jujuy",-24.185556, -65.299444)])
+ciudades.extend([ciudad("Santa Rosa",-36.620278, -64.290556)])
+ciudades.extend([ciudad("La Rioja",-29.413056, -66.855833)])
+ciudades.extend([ciudad("Mendoza",-32.883333, -68.833333)])
+ciudades.extend([ciudad("Posadas",-27.366667, -55.896944)])
+ciudades.extend([ciudad("Neuquén",-38.95735, -68.045533)])
+ciudades.extend([ciudad("Viedma",-40.8, -63)])
+ciudades.extend([ciudad("Salta",-24.788333, -65.410556)])
+ciudades.extend([ciudad("San Juan",-31.5375, -68.536389)])
+ciudades.extend([ciudad("San Luis",-33.277222, -66.3525)])
+ciudades.extend([ciudad("Rio Gallegos",-51.633333, -69.233333)])
+ciudades.extend([ciudad("Santa Fe",-31.633333, -60.7)])
+ciudades.extend([ciudad("Santiago del Estero",-27.784444, -64.266944)])
+ciudades.extend([ciudad("Ushuaia",-54.807222, -68.304444)])
+ciudades.extend([ciudad("S. M. de Tucumán",-26.816667, -65.216667)])
+for x in range (0,len(ciudades)): #No se si va a servir de algo
+    ciudades[x].setId(x)
+    
+
+#variables
+ciclos=300
+cantIndividuos=10
+prob_crossover=0.75
+prob_mutacion=0.05
+
+
+
+print ("Ingrese num ciudad inicial (0-22)")
+#num = int(input())
+num = 0
+
+poblacion = crearPoblacionInicial(num)
+calcularFitness(poblacion)
+print()
+
+"""Elite
+poblacion = sorted(poblacion, key = lambda object : object.distancia) 
+    hijos=[]
+    hijos.extend([poblacion[0]])
+    hijos.extend([poblacion[1]])
+    """
