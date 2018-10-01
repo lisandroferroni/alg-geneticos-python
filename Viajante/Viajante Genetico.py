22d3686e89dd0c5eed297213d01ce0fceb1e869e
lisandroferroni <lisandro.ferroni@gmail.com>
2018-10-01 04:22:34 -0300

    Algoritmo principal (fitness2, ruleta, seleccion)

--- Viajante/Viajante Genetico.py
+++ Viajante/Viajante Genetico.py
@@ -68,6 +68,21 @@ def crearPoblacionInicial(num):
         poblacionInicial.extend([Cromosoma(trayectoria)])   #Crea nuevo cromosoma con trayectoria aleatoria
     return poblacionInicial         #Devuelve lista con @cantIndividuos cromosomas 
 
+def calcularElite(poblacion):
+    poblacion = sorted(poblacion, key = lambda object : object.distancia) 
+    hijos=[]
+    hijos.extend([poblacion[0]])
+    hijos.extend([poblacion[1]])
+    return hijos
+
+def crearRuleta(poblacion):
+    ruleta=[]
+    calcularFitness2(poblacion)          #asigna fitness con valores enteros.
+    for x in range (0,len(poblacion)):  #Por cada cromosoma:
+        for _ in range (0,poblacion[x].getFitness()): 
+            ruleta.extend([poblacion[x].getTrayectoria()])
+    return ruleta
+
 def calcularFitness (poblacion):
     total=0.0
     for x in range(0,len(poblacion)):
@@ -75,19 +90,27 @@ def calcularFitness (poblacion):
     for x in range(0,len(poblacion)):
         poblacion[x].setFitness(int(round(100/poblacion[x].getDistancia()/total)))
 
-def crearRuleta(poblacion):
-    ruleta=[]
-    calcularFitness(poblacion)
-    for x in range (0,len(poblacion)):
-        for _ in range (0,poblacion[x].getFitness()):
-            ruleta.extend([poblacion[x].getTrayectoria()])
-    return ruleta
+def calcularFitness2 (poblacion):
+    sumaDistancias = 0.0
+    for dist in poblacion:
+        sumaDistancias = sumaDistancias + dist.getDistancia()
+
+    puntaje = []
+    sumaPuntajes = 0.0
+    for x in range(0, len(poblacion)):
+        aux = 1 - (poblacion[x].getDistancia()/sumaDistancias)
+        puntaje.extend([aux])
+        sumaPuntajes = sumaPuntajes + aux
+
+    for x in range(0, len(poblacion)):
+        poblacion[x].setFitness(int(round(100*puntaje[x]/sumaPuntajes)))
+
 
 #crossover ciclico
 def ciclicCrossover(padrex1,padrex2):
     posActual=0 #variable aux 
-    padre1 = padrex1.getTrayectoria
-    padre2 = padrex2.getTrayectoria
+    padre1 = padrex1.getTrayectoria()
+    padre2 = padrex2.getTrayectoria()
     hijo1=[] #instancio los pibes
     hijo2=[] 
 
@@ -117,54 +140,72 @@ def ciclicCrossover(padrex1,padrex2):
     hijox2= Cromosoma(hijo2)
     return hijox1,hijox2
 
-ciudades=[]
-ciudades.extend([ciudad("Buenos Aires",-34.599722, -58.381944)])
-ciudades.extend([ciudad("Córdoba",-31.416667, -64.183333)])
-ciudades.extend([ciudad("S. F. del valle de Catamarca",-28.468611, -65.779167)])
-ciudades.extend([ciudad("Resistencia",-27.451389, -58.986667)])
-ciudades.extend([ciudad("Rawson",-43.3, -65.1)])
-ciudades.extend([ciudad("Corrientes",-27.483333, -58.816667)])
-ciudades.extend([ciudad("Paraná",-31.744444, -60.5175)])
-ciudades.extend([ciudad("Formosa",-26.184722, -58.175833)])
-ciudades.extend([ciudad("San S. de Jujuy",-24.185556, -65.299444)])
-ciudades.extend([ciudad("Santa Rosa",-36.620278, -64.290556)])
-ciudades.extend([ciudad("La Rioja",-29.413056, -66.855833)])
-ciudades.extend([ciudad("Mendoza",-32.883333, -68.833333)])
-ciudades.extend([ciudad("Posadas",-27.366667, -55.896944)])
-ciudades.extend([ciudad("Neuquén",-38.95735, -68.045533)])
-ciudades.extend([ciudad("Viedma",-40.8, -63)])
-ciudades.extend([ciudad("Salta",-24.788333, -65.410556)])
-ciudades.extend([ciudad("San Juan",-31.5375, -68.536389)])
-ciudades.extend([ciudad("San Luis",-33.277222, -66.3525)])
-ciudades.extend([ciudad("Rio Gallegos",-51.633333, -69.233333)])
-ciudades.extend([ciudad("Santa Fe",-31.633333, -60.7)])
-ciudades.extend([ciudad("Santiago del Estero",-27.784444, -64.266944)])
-ciudades.extend([ciudad("Ushuaia",-54.807222, -68.304444)])
-ciudades.extend([ciudad("S. M. de Tucumán",-26.816667, -65.216667)])
-for x in range (0,len(ciudades)): #No se si va a servir de algo
-    ciudades[x].setId(x)
-    
+def mutar (padre):
+    return padre
+
+def cargaCiudades():
+    ciudades=[]
+    ciudades.extend([ciudad("Buenos Aires",-34.599722, -58.381944)])
+    ciudades.extend([ciudad("Córdoba",-31.416667, -64.183333)])
+    ciudades.extend([ciudad("S. F. del valle de Catamarca",-28.468611, -65.779167)])
+    ciudades.extend([ciudad("Resistencia",-27.451389, -58.986667)])
+    ciudades.extend([ciudad("Rawson",-43.3, -65.1)])
+    ciudades.extend([ciudad("Corrientes",-27.483333, -58.816667)])
+    ciudades.extend([ciudad("Paraná",-31.744444, -60.5175)])
+    ciudades.extend([ciudad("Formosa",-26.184722, -58.175833)])
+    ciudades.extend([ciudad("San S. de Jujuy",-24.185556, -65.299444)])
+    ciudades.extend([ciudad("Santa Rosa",-36.620278, -64.290556)])
+    ciudades.extend([ciudad("La Rioja",-29.413056, -66.855833)])
+    ciudades.extend([ciudad("Mendoza",-32.883333, -68.833333)])
+    ciudades.extend([ciudad("Posadas",-27.366667, -55.896944)])
+    ciudades.extend([ciudad("Neuquén",-38.95735, -68.045533)])
+    ciudades.extend([ciudad("Viedma",-40.8, -63)])
+    ciudades.extend([ciudad("Salta",-24.788333, -65.410556)])
+    ciudades.extend([ciudad("San Juan",-31.5375, -68.536389)])
+    ciudades.extend([ciudad("San Luis",-33.277222, -66.3525)])
+    ciudades.extend([ciudad("Rio Gallegos",-51.633333, -69.233333)])
+    ciudades.extend([ciudad("Santa Fe",-31.633333, -60.7)])
+    ciudades.extend([ciudad("Santiago del Estero",-27.784444, -64.266944)])
+    ciudades.extend([ciudad("Ushuaia",-54.807222, -68.304444)])
+    ciudades.extend([ciudad("S. M. de Tucumán",-26.816667, -65.216667)])
+    for x in range (0,len(ciudades)): #No se si va a servir de algo
+        ciudades[x].setId(x)
+    return ciudades
+
+def algoritmoPrincipal(poblacion):
+    hijos = calcularElite(poblacion)        #pasan los 2 cromosomas con menor distancia
+    ruleta = crearRuleta(poblacion)         #Ruleta es lista de trayectorias
+    for _ in range (0,int((len(poblacion)-len(hijos))/2)):
+
+        padre1 = random.choice(ruleta)  #padre 1 y 2 son trayectorias seleccionadas
+        padre2 = random.choice(ruleta)  #al azar de la ruleta
+        
+        if (random.random()<=prob_crossover):       #probabilidad de crossover
+            #padre1,padre2 = ciclicCrossover (padre1,padre2)
+            pass    #borrar esta linea cuando ande el crossover
+        if (random.random()<=prob_mutacion):     #probabilidad de mutacion
+            padre1 = mutar(padre1)              
+            padre2 = mutar(padre2)
+        
+        hijos.extend([Cromosoma(padre1),Cromosoma(padre2)])
+
+    return hijos
+
 
 #variables
-ciclos=300
+ciclos=1
 cantIndividuos=10
 prob_crossover=0.75
 prob_mutacion=0.05
 
-
-
+#Main
+ciudades = cargaCiudades()
 print ("Ingrese num ciudad inicial (0-22)")
 #num = int(input())
 num = 0
 
 poblacion = crearPoblacionInicial(num)
-calcularFitness(poblacion)
 
+for x in range (0,ciclos):
+    hijos = algoritmoPrincipal(poblacion)
 print()
-
-"""Elite
-poblacion = sorted(poblacion, key = lambda object : object.distancia) 
-    hijos=[]
-    hijos.extend([poblacion[0]])
-    hijos.extend([poblacion[1]])
-    """
